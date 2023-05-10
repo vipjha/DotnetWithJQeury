7466dd4562634e6dc5ced032d7555e7279e05063
Vj <vipinjha@hotmail.com>
2023-05-10 13:43:29 +0530

    Initial

---
+++ DotnetCoreJQuery/DotnetCoreJQuery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotnetCoreJQuery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
